0368b55b229cff707dcffeddc462e9adfa56b38b
Olena <alchimeraa@gmail.com>
2024-11-12 23:01:48 +0200

    components

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,6 +1,16 @@
 import styles from './Contact.module.css';
 
 const Contact = ({ id, name, number, onDelete }) => (
+/* <li className={styles.contact}>
+  <div className="contactInfo">
+    <span className="name">{name}</span>
+    <span className="number">{number}</span>
+  </div>
+  <button onClick={() => onDelete(id)}>Delete</button>
+</li> */
+
+
+
   <li className={styles.contact}>
     <span>{name}: {number}</span>
     <button onClick={() => onDelete(id)}>Delete</button>
